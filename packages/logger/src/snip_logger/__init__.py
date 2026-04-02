9cfff64e02dc3b4cb0e3908072b2aff5bffc9d07
dkhodos <dkhodos94@gmail.com>
2026-04-02 17:33:33 +0300

    refactor: rename logger package modules to drop underscore prefix

--- packages/logger/src/snip_logger/__init__.py
+++ packages/logger/src/snip_logger/__init__.py
@@ -1,8 +1,8 @@
 """snip-logger: structured logging for Snip services."""
 
-from snip_logger._config import configure_logging
-from snip_logger._logger import get_logger
-from snip_logger._middleware import logging_middleware
+from snip_logger.config import configure_logging
+from snip_logger.logger import get_logger
+from snip_logger.middleware import logging_middleware
 
 __all__ = [
     "configure_logging",
